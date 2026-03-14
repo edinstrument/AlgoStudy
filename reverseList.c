37abd1e991eac9668522da1fadfedf94a9bc7e14
YuNam Lee <qw705@naver.com>
2026-03-15 03:28:01 +0900

    Leetcode 206(reversedLL)

---
+++ reverseList.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    int val;
+    struct ListNode* Temp = NULL;
+    struct ListNode* pre = NULL;
+    struct ListNode* live = head;
+    while (live != NULL)
+    {
+        Temp = live->next;
+        live->next = pre;
+        pre = live;
+        live = Temp;
+    }
+    return pre;
+}
+
